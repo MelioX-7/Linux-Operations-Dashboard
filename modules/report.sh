05d78f7045ee35a4a8147bb9ff429e9280df9f18
Kathir Viswa <kathirviswa78@gmail.com>
2026-09-09 23:51:09 +0530

    CPU Usage

--- modules/report.sh
+++ modules/report.sh
@@ -18,5 +18,22 @@ generate_report() {
 
     echo >> "$report_file"
 
+    echo "Kernel: $(uname -r)" >> "$report_file"
+
+    echo >> "$report_file"
+
+    echo "CPU INFORMATION" >> "$report_file"
+    echo "------------------------------------" >> "$report_file"
+    echo "CPU Cores: $(nproc)" >> "$report_file"
+    echo "CPU Usage: $(top -bn1 | grep "%Cpu" | awk '{for(i=1;i<=NF;i++) if($i ~ /id,/) {idle=$(i-1); print 100-idle}}')%" >> "$report_file"
+    echo "Load Average: $(uptime | awk -F'load average: ' '{print $2}')" >> "$report_file"
+
+    echo >> "$report_file"
+
+
+
+
     echo "Report saved to: $report_file"
+
+
 }
